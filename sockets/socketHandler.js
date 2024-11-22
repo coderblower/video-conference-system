f3882d733c4ee90d43d90b54148017068b6467d0
CodeBlower <saifulrmail@gmail.com>
2024-11-22 16:05:59 +0600

    multiuser

--- sockets/socketHandler.js
+++ sockets/socketHandler.js
@@ -3,26 +3,57 @@ const socketIo = require('socket.io');
 function setupSocket(server) {
     const io = socketIo(server);
 
-    // Handle socket connections
+    // Store users in rooms
+    const rooms = {};
+
     io.on('connection', (socket) => {
-        console.log('A user connected');
-    
-        // Join the room when the user accesses a URL like '/index.html/{roomId}'
+        console.log('A user connected:', socket.id);
+
+        // Join a room
         socket.on('join-room', (roomId) => {
-            
-            socket.join(roomId); // Join the room
-            
+            socket.join(roomId);
+
+            // Add user to room
+            if (!rooms[roomId]) {
+                rooms[roomId] = [];
+            }
+            rooms[roomId].push(socket.id);
+
+            console.log(`User ${socket.id} joined room ${roomId}`);
+
+            // Notify other users in the room
+            socket.to(roomId).emit('new-user', socket.id);
         });
-    
+
         // Handle signaling messages (offer/answer/ICE candidates)
         socket.on('message', (data) => {
-            console.log('hello', data);
-            // Forward the signaling message to the other peer in the room
-            socket.to(data.roomId).emit('message', data);
+            const { roomId, to } = data;
+
+            if (to) {
+                // Forward message to a specific user
+                io.to(to).emit('message', { ...data, from: socket.id });
+            } else {
+                // Broadcast to all users in the room except sender
+                socket.to(roomId).emit('message', { ...data, from: socket.id });
+            }
         });
-    
+
+        // Handle disconnection
         socket.on('disconnect', () => {
-            console.log('A user disconnected');
+            console.log('A user disconnected:', socket.id);
+
+            // Remove user from all rooms they joined
+            for (const roomId in rooms) {
+                rooms[roomId] = rooms[roomId].filter((id) => id !== socket.id);
+
+                // Notify other users in the room
+                socket.to(roomId).emit('user-left', socket.id);
+
+                // Clean up empty rooms
+                if (rooms[roomId].length === 0) {
+                    delete rooms[roomId];
+                }
+            }
         });
     });
 
