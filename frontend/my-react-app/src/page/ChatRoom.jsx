1b288606a0a75d01801e850888e266e80a6f13f6
saiful110816 <saiful110816@acc.edu.bd>
2024-12-29 15:49:36 +0600

    update 3:49

--- frontend/my-react-app/src/page/ChatRoom.jsx
+++ frontend/my-react-app/src/page/ChatRoom.jsx
@@ -27,6 +27,7 @@ const ChatRoom = () => {
     const localVideoRef = useRef(null);
     const [remoteVideos, setRemoteVideos] = useState({});
     const { localStream, setLocalStream } = useLocalStream();
+    const [ localStream2, setLocalStream2 ] = useState(null);
    
    
 
@@ -43,15 +44,16 @@ const ChatRoom = () => {
             try {
                 socket.emit("join-room", roomId);
     
-                // Await the stream from streamService
-                // if(!localStream){
-                //     const stream = await streamService();
-                // if (stream) {
-                //     setLocalStream(stream);
-                // } else {
-                //     console.log("No stream available. Falling back to avatar or other alternatives.");
-                // }
-                // }
+                console.log('frie', localStream2)
+                if(!localStream2){
+                    const stream = await streamService();
+                        console.log('rid')
+                    if (stream) {
+                        setLocalStream2(stream);
+                    } else {
+                        console.log("No stream available. Falling back to avatar or other alternatives.");
+                    }
+                }
     
                 // Socket event for when a new user joins
                 socket.on("new-user", async (userId) => {
@@ -222,14 +224,14 @@ const ChatRoom = () => {
 
            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
                 
-                {localStream && (()=>{
+                { localStream2  && (()=>{
                     
-                    const hasVideoTrack = localStream?.getVideoTracks()?.length > 0;
+                    const hasVideoTrack = localStream2?.getVideoTracks()?.length > 0;
                     
                     return hasVideoTrack ? (
-                    <VideoCard key={userName} stream={localStream} muted = {true} title={`Video - ${userName}`} />
+                    <VideoCard key={userName} stream={localStream2} muted = {true} title={`Video - ${userName}`} />
                     ) : (
-                    <AudioCard key={userName} stream={localStream} muted = {true} title={`Audio - ${userName}`} description="No video available" />
+                    <AudioCard key={userName} stream={localStream2} muted = {true} title={`Audio - ${userName}`} description="No video available" />
                     );
                 })()}
                  
