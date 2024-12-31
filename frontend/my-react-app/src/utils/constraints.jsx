fa1d5582c0488daf1a5334f683857ebba72a1dce
saiful110816 <saiful110816@acc.edu.bd>
2024-12-31 17:21:54 +0600

    fake url

--- frontend/my-react-app/src/utils/constraints.jsx
+++ frontend/my-react-app/src/utils/constraints.jsx
@@ -1,7 +1,7 @@
 export const servers = {
     iceServers: [
         {
-            urls: "stun:103.174.152.203:3478", // Replace with your TURN server
+            urls: "stun:example.comsf:3478", // Replace with your TURN server
            
         }
     ],
