f8b81cd13bf9211a2e41c5bc91b924cb129e4198
saiful110816 <saiful110816@acc.edu.bd>
2025-01-19 11:25:44 +0600

    fix

--- frontend/my-react-app/src/utils/constraints.jsx
+++ frontend/my-react-app/src/utils/constraints.jsx
@@ -1,8 +1,14 @@
 export const servers = {
     iceServers: [
-        {
-            urls: "stun:103.174.152.203:3478", // Replace with your TURN server
-           
-        }
+        export const servers = {
+            iceServers: [
+                { urls: "stun:stun.l.google.com:19302" }, // Public STUN server
+                {
+                    urls: "stun:103.174.152.203:3478", // Replace with your TURN server
+                   
+                }
+            ],
+        };
+        
     ],
 };
